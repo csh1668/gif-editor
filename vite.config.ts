c84e4db1b427c9b70b0cf895cd4da856ffc3939a
csh1668 <tjgus1668@naver.com>
2025-09-03 19:50:26 +0900

    chore(config): update project config and tooling

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
-
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    exclude: [
+      '@ffmpeg/ffmpeg',
+      '@ffmpeg/util',
+      '@ffmpeg/core',
+      '@ffmpeg/core-st',
+    ],
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
